e64e759c76963bbea6035793c0f5f8418d2ed991
joeljosephreji <joeljosephreji@gmail.com>
2023-04-21 00:23:16 +0530

    Configuring neorg

--- lua/user/packer.lua
+++ lua/user/packer.lua
@@ -32,23 +32,44 @@ return require('packer').startup(function(use)
         'VonHeikemen/lsp-zero.nvim',
         branch = 'v2.x',
         requires = {
-            -- LSP Support
-            {'neovim/nvim-lspconfig'},             -- Required
-            {                                      -- Optional
-                'williamboman/mason.nvim',
-                run = function()
-                    pcall(vim.cmd, 'MasonUpdate')
-                end,
-            },
-            {'williamboman/mason-lspconfig.nvim'}, -- Optional
+                -- LSP Support
+                {'neovim/nvim-lspconfig'},             -- Required
+                {                                      -- Optional
+                    'williamboman/mason.nvim',
+                    run = function()
+                        pcall(vim.cmd, 'MasonUpdate')
+                    end,
+                },
+                {'williamboman/mason-lspconfig.nvim'}, -- Optional
 
-            -- Autocompletion
-            {'hrsh7th/nvim-cmp'},     -- Required
-            {'hrsh7th/cmp-nvim-lsp'}, -- Required
-            {'L3MON4D3/LuaSnip'},     -- Required
-        }
+                -- Autocompletion
+                {'hrsh7th/nvim-cmp'},     -- Required
+                {'hrsh7th/cmp-nvim-lsp'}, -- Required
+                {'L3MON4D3/LuaSnip'},     -- Required
+        },
     })
 
     use('tpope/vim-fugitive')
+
+    use {
+            "nvim-neorg/neorg",
+            config = function()
+                require('neorg').setup {
+                    load = {
+                        ["core.defaults"] = {}, -- Loads default behaviour
+                        ["core.concealer"] = {}, -- Adds pretty icons to your documents
+                        ["core.dirman"] = { -- Manages Neorg workspaces
+                        config = {
+                            workspaces = {
+                                notes = "~/notes",
+                            },
+                        },
+                    },
+                },
+            }
+        end,
+        run = ":Neorg sync-parsers",
+        requires = "nvim-lua/plenary.nvim",
+    }
 
 end)
