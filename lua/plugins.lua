75aabd42dbcd1fedc623a207d9e4863b53d50c22
Joel Joseph Reji <joeljosephreji@gmail.com>
2024-11-19 15:32:14 +1100

    using transparency in catpuccin

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -5,6 +5,11 @@ return {
         name = "catppuccin",
         priority = 1000,
         config = function()
+            require("catppuccin").setup({
+                transparent_background = true,
+            })
+
+            -- setup must be called before loading
             vim.cmd.colorscheme('catppuccin')
         end
     },
