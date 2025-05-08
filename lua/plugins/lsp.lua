e023a01996596c74c735d9586bf3309dfd60abd5
Joel Joseph Reji <joeljosephreji@gmail.com>
2025-05-09 00:00:18 +1000

    fixing the mason version for now, update later

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -5,8 +5,8 @@ return {
     'neovim/nvim-lspconfig',
     dependencies = {
         -- Automatically install LSPs to stdpath for neovim
-        'williamboman/mason.nvim',
-        'williamboman/mason-lspconfig.nvim',
+        { 'williamboman/mason.nvim', version = "^1.0.0" },
+        { 'williamboman/mason-lspconfig.nvim', version = "^1.0.0" },
 
         -- Useful status updates for LSP
         -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
