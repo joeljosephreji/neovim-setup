a642355c1534aed3d2b4d2b56c4392c67ff3647a
Joel Joseph Reji <joeljosephreji@gmail.com>
2025-06-11 13:53:09 +1000

    removed quick fix/location list keybinds and replaced with diagnostic list

--- lua/remaps.lua
+++ lua/remaps.lua
@@ -18,13 +18,10 @@ vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
 -- vim.keymap.set("n", "<leader><leader>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
-vim.keymap.set("n", "<leader>qk", "<cmd>cprev<CR>zz")
-vim.keymap.set("n", "<leader>qj", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<leader>qh", "<cmd>lprev<CR>zz")
-vim.keymap.set("n", "<leader>ql", "<cmd>lnext<CR>zz")
-
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 vim.keymap.set("n", "<leader>cd", ":cd %:p:h<CR>:pwd<CR>", { silent = true })
+
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
