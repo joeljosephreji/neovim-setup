1b0bd479b0149b89804c6a6a064314f669abb1b4
Joel Joseph Reji <joeljosephreji@gmail.com>
2025-06-13 16:52:51 +1000

    will include scratch once the delete functionality comes

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -6,9 +6,12 @@ return {
     opts = {
         lazygit = { enabled = true },   -- for git stuff
         indent = { enabled = true },    -- for knowing the indent level
+        -- scratch = { enabled = true },   -- for having scratch buffers
     },
     keys = {
         { "<leader>gg", function() Snacks.lazygit() end, desc = "Lazygit" },
+        -- { "<leader>.",  function() Snacks.scratch() end, desc = "Toggle Scratch Buffer" },
+        -- { "<leader>S",  function() Snacks.scratch.select() end, desc = "Select Scratch Buffer" },
     }
 }
 
