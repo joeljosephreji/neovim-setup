871c69117a50d54d0f58a354839b4d28d100d43b
Joel Joseph Reji <joeljosephreji@gmail.com>
2025-06-13 17:08:28 +1000

    added buffer delete as well as lsp rename files

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -7,11 +7,16 @@ return {
         lazygit = { enabled = true },   -- for git stuff
         indent = { enabled = true },    -- for knowing the indent level
         -- scratch = { enabled = true },   -- for having scratch buffers
+        bufdelete = { enabled = true }, -- for deleting buffers
+        rename = { enabled = true },    -- for renaming files
     },
     keys = {
         { "<leader>gg", function() Snacks.lazygit() end, desc = "Lazygit" },
         -- { "<leader>.",  function() Snacks.scratch() end, desc = "Toggle Scratch Buffer" },
         -- { "<leader>S",  function() Snacks.scratch.select() end, desc = "Select Scratch Buffer" },
+        { "<leader>bdc", function() Snacks.bufdelete() end, desc = "Buffer Delete Current" },
+        { "<leader>bdo", function() Snacks.bufdelete.others() end, desc = "Buffer Delete Others" },
+        { "<leader>cR", function() Snacks.rename.rename_file() end, desc = "Rename File" },
     }
 }
 
