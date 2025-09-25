14224d0899bc81b182c7461786c025b324c40519
Joel Joseph Reji <joeljosephreji@gmail.com>
2025-09-26 08:55:25 +1000

    removing autosave limitation on cpp and adding hpp file to formatter

--- lua/plugins/format.lua
+++ lua/plugins/format.lua
@@ -18,7 +18,8 @@ return {
 			-- Disable "format_on_save lsp_fallback" for languages that don't
 			-- have a well standardized coding style. You can add additional
 			-- languages here or re-enable it for the disabled ones.
-			local disable_filetypes = { c = true, cpp = true }
+			-- local disable_filetypes = { c = true, cpp = true }
+			local disable_filetypes = {}
 			if disable_filetypes[vim.bo[bufnr].filetype] then
 				return nil
 			else
@@ -34,6 +35,7 @@ return {
 			bash = { "shfmt" },
 			nix = { "nixfmt" },
 			cpp = { "clang-format" },
+			hpp = { "clang-format" },
 			-- Conform can also run multiple formatters sequentially
 			-- python = { "isort", "black" },
 			--
