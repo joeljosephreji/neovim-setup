11f54d06c65acefafad86b354c95850816f4421e
Joel Joseph Reji <joeljosephreji@gmail.com>
2026-06-12 15:19:27 +1000

    removing python provider stuff, fixing treesitter to work

--- lua/globals.lua
+++ lua/globals.lua
@@ -1,4 +1,4 @@
 -- for python neovim virtual environment
-vim.g.python3_host_prog = require("python-neovim-venv-path")
+-- vim.g.python3_host_prog = require("python-neovim-venv-path")
 
 vim.g.have_nerd_font = true
